d6dc40e538a443292b2a4a2a8ab81d80173cc04d
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-05-21 14:49:00 +0900

    configを別ファイルに分離

--- js/app.js
+++ js/app.js
@@ -1,42 +1,4 @@
-const config = {
-  icando: [
-    {
-      title: '言語',
-      items: [
-        'HTML5', 'CSS3', 'JavaScript', 'C', 'Csharp', 'Java', 'VB', 'PHP', 'Ruby', 'Python',
-        'Node', 'Go', 'Kotlin', 'TypeScript', 'Sass', 'Less', 'Swift', 'Japanese'
-      ]
-    },
-    {
-      title: 'フレームワーク',
-      items: [
-        'jQuery', 'BootStrap', 'Laravel', 'Rails', 'Xamarin', 'dotNET', 'SpringBoot',
-        'Vue', 'Nuxt', 'React', 'Express'
-      ]
-    },
-    {
-      title: 'データベース',
-      items: [
-        'MySQL', 'PostgreSQL', 'Oracle', 'SQLite', 'Redis', 'Firebase', 'Elasticsearch'
-      ]
-    },
-    {
-      title: 'OS',
-      items: [
-        'Windows', 'macOS', 'ubuntu', 'CentOS', 'elementaryOS', 'KaliLinux', 'openSUSE'
-      ]
-    },
-    {
-      title: 'その他ツール類',
-      items: [
-        'Git', 'SVN', 'Docker', 'Kubernetes', 'webpack', 'Jenkins', 'Redmine', 'AWS', 'Azure'
-      ]
-    }
-
-  ]
-}
-
-window.onload = () => {
+window.addEventListener('load', () => {
 
   const skillsWrapper = document.getElementById('js-skills-wrapper')
 
@@ -44,34 +6,41 @@ window.onload = () => {
   config.icando.forEach((ary) => {
     const skillsContainer = document.createElement('div')
           skillsContainer.setAttribute('class', 'skills-container')
+
     const skillTitle = document.createElement('h6')
           skillTitle.setAttribute('class', 'skills-title')
+
     const skillInnerTitle = document.createElement('span')
           skillInnerTitle.textContent = ary.title
-          skillTitle.appendChild(skillInnerTitle)
+
     const skills = document.createElement('div')
           skills.setAttribute('class', 'skills')
-          skillsContainer.appendChild(skillTitle)
-          skillsContainer.appendChild(skills)
+
+    skillTitle.appendChild(skillInnerTitle)
+    skillsContainer.appendChild(skillTitle)
+    skillsContainer.appendChild(skills)
 
     ary.items.forEach((item) => {
       const skill = document.createElement('div')
             skill.setAttribute('class', 'skill')
+
       const skillIcon = document.createElement('div')
             skillIcon.setAttribute('class', 'skill-icon')
+
       const skillImg = document.createElement('img')
             skillImg.setAttribute('src', `${skillIconPath}${item}.png`)
             skillImg.setAttribute('alt', item)
-            skillIcon.appendChild(skillImg)
+
       const skillLabel = document.createElement('div')
             skillLabel.setAttribute('class', 'skill-label')
             skillLabel.textContent = item
+
+      skillIcon.appendChild(skillImg)
       skill.appendChild(skillIcon)
       skill.appendChild(skillLabel)
-
       skills.appendChild(skill)
     })
     skillsWrapper.appendChild(skillsContainer)
   })
 
-}
+})
---
+++ js/config.js
@@ -0,0 +1,37 @@
+const config = {
+  icando: [
+    {
+      title: '言語',
+      items: [
+        'HTML5', 'CSS3', 'JavaScript', 'C', 'Csharp', 'Java', 'VB', 'PHP', 'Ruby', 'Python',
+        'Node', 'Go', 'Kotlin', 'TypeScript', 'Sass', 'Less', 'Swift', 'Japanese'
+      ]
+    },
+    {
+      title: 'フレームワーク',
+      items: [
+        'jQuery', 'BootStrap', 'Laravel', 'Rails', 'Xamarin', 'dotNET', 'SpringBoot',
+        'Vue', 'Nuxt', 'React', 'Express'
+      ]
+    },
+    {
+      title: 'データベース',
+      items: [
+        'MySQL', 'PostgreSQL', 'Oracle', 'SQLite', 'Redis', 'Firebase', 'Elasticsearch'
+      ]
+    },
+    {
+      title: 'OS',
+      items: [
+        'Windows', 'macOS', 'ubuntu', 'CentOS', 'elementaryOS', 'KaliLinux', 'OpenSUSE'
+      ]
+    },
+    {
+      title: 'その他ツール類',
+      items: [
+        'Git', 'SVN', 'Docker', 'Kubernetes', 'webpack', 'Jenkins', 'Redmine', 'AWS', 'Azure'
+      ]
+    }
+
+  ]
+}
